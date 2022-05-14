b735fac7c57b24734ff668ee771382b98457aac2
Ehis Edemakhiota <edemaehiz@gmail.com>
2022-05-14 05:25:51 +0100

    added files

--- src/main/java/com/technophiles/diaryapp/security/jwt/SecurityConstants.java
+++
@@ -1,6 +0,0 @@
-package com.technophiles.diaryapp.security.jwt;
-
-public class SecurityConstants {
-    final static String SIGNING_KEY = "SECRET_KEY";
-    final static String AUTHORITIES_KEY = "ROLE_";
-}
